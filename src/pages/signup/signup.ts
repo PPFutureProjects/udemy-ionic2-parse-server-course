dc65317f3cc9f95f44a0887dc17e3fed49859c23
bdg <basdegroot.91@gmail.com>
2017-02-25 23:05:06 +0100

    Finished lecture 17

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, AlertController, NavParams } from 'ionic-angular';
 import { User } from '../../user-model';
 import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-signup',
@@ -18,8 +19,14 @@ export class SignupPage {
 
   confirmPassword: string;
   url: string;
+  headers: Headers;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public http: Http, public navParams: NavParams) {
+    // Initialize the headers object
+    this.headers = new Headers();
+    this.headers.append("X-Parse-Application-Id", "AppId1");
+    this.headers.append("X-Parse-Master-Key", "masterKey");
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
@@ -29,4 +36,30 @@ export class SignupPage {
     this.navCtrl.pop();
   }
 
+/**
+ * This method is responsible for handeling the sing up process.
+ */
+  signup() {
+    // Check if both passwords match
+    if (this.user.password != this.confirmPassword) {
+      this.alertCtrl.create({
+        title: "Error",
+        message: "Passwords do not match, please try again.",
+        buttons: ['OK']
+      }).present();
+      return;
+    }
+
+    this.url = "https://parse-with-ionic-bdegroot.c9users.io/app1/users"
+
+    // Make Http post request
+    this.http.post(this.url, this.user, {headers: this.headers})
+      .map(res => res.json())
+      .subscribe(res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      })
+  }
 }
