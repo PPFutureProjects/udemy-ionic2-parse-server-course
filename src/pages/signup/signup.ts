60585d5e5681f5ba3a063e0afceecee3dd316ab1
bdg <basdegroot.91@gmail.com>
2017-02-25 22:35:44 +0100

    Finished lecture 16

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { User } from '../../user-model';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html'
 })
 export class SignupPage {
+
+  user: User = {
+    name: "",
+    username: "",     // required
+    email: "",
+    password: ""      // required
+  };
+
+  confirmPassword: string;
+  url: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
