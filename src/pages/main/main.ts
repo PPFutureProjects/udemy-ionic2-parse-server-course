2336a699f97629e4b0d9ff65bb2ae83abab970be
bdg <basdegroot.91@gmail.com>
2017-02-26 13:49:04 +0100

    Finished lecture 21

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Main page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-main',
   templateUrl: 'main.html'
@@ -15,8 +9,8 @@ export class MainPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MainPage');
+  showAddDialog() {
+    
   }
 
 }
