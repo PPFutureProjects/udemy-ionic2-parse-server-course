786e374ac1469a9d4a4031a5769a96094bd9b8b4
bdg <basdegroot.91@gmail.com>
2017-02-26 15:12:47 +0100

    Finished lecture 28

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -67,7 +67,7 @@ export class MainPage {
                 buttons: [{
                   text: "OK",
                   handler: () => {
-
+                    this.getFriends();
                   }
                 }]
               }).present();
